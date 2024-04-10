175c50f464aee0785fc440dc8a8162c295037efe
SSFSKIM <kimmi@ssfs.org>
2024-04-10 09:02:39 -0400

    Initial Commit

---
+++ login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  login
+//
+//  Created by Steve on 4/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    @IBOutlet var forgotPass: UIButton!
+    @IBOutlet var forgotUser: UIButton!
+    @IBAction func loyin(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
